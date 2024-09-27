19aef08010097c97f43d1a6db540139d75d9a57e
Fer Alderetes <feralderetes@gmail.com>
2024-09-27 13:10:33 +0200

    Added footer with project links

--- src/App.js
+++ src/App.js
@@ -5,6 +5,36 @@ function App() {
   return (
     <div className="App">
       <Dictionary />
+      <footer>
+        Coded by{" "}
+        <a
+          href="https://github.com/feralderetes/"
+          target="_blank"
+          rel="noreferrer"
+          title="Github profile"
+        >
+          Fer Alderetes
+        </a>{" "}
+        👩🏻‍💻, open-sourced on{" "}
+        <a
+          href="https://github.com/feralderetes/dictionary-by-fer/"
+          target="_blank"
+          rel="noreferrer"
+          title="Github repository"
+        >
+          Github
+        </a>{" "}
+        and hosted on
+        <a
+          href="https://dictionary-by-fer.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+          title="Netlify hosted website"
+        >
+          {" "}
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }

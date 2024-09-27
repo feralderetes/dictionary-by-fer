e087093dfd54453ace1157adc45c2890d68a1998
Fer Alderetes <feralderetes@gmail.com>
2024-09-27 11:35:10 +0200

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <h1>Dictionary</h1>
+      <Dictionary />
     </div>
   );
 }

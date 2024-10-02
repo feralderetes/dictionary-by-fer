fbcdeb970dd81f528c31a789aa112e98ef364c4c
Fer Alderetes <feralderetes@gmail.com>
2024-10-02 11:39:36 +0200

    Added bootstrap and created Photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleImagesResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
+  function handleDictionaryResponse(response) {
     setResults(response.data);
   }
 
@@ -16,7 +22,13 @@ export default function Dictionary() {
 
     const apiKey = "4a6baff0aba2ofc3b32f2f5atce330d1";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    const pexelsApiKey =
+      "usAusOA01rbv2qubFjyFWeVmsG91XLcAezTCuUcI0rKvHjL4Crsh6yce";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handleImagesResponse);
   }
 
   function handleKeywordChange(event) {
@@ -35,9 +47,10 @@ export default function Dictionary() {
             placeholder="Search for a word"
           ></input>
         </form>
-        <div className="hint">e.g., sunset, sunrise, world, nature... </div>
+        <div className="hint">e.g., puppy, kitten, flower, biscuit... </div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
---
+++ src/Photos.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <section className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div key={index} className="col-4">
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img
+                    src={photo.src.tiny}
+                    alt={photo.alt}
+                    className="img-fluid img-thumbnail"
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
+}

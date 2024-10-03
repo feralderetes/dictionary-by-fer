d9a5f0d73ca0548002331e5f0e0db336d9fcbf2b
Fer Alderetes <feralderetes@gmail.com>
2024-10-03 11:46:28 +0200

    Fixed responsiveness for error message

--- src/Results.js
+++ src/Results.js
@@ -23,9 +23,9 @@ export default function Results(props) {
       );
     } else {
       return (
-        <section>
+        <section className="Error">
           <h2>Oops!</h2>
-          <div>
+          <div className="message">
             Could not find a definition for that word. Check the spelling in
             English or try a different word.
           </div>
